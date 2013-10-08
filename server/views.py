6b3f378a425510f103bdc7edb42202c9d94f935c
Daniele Zanni <syymza@gmail.com>
2013-10-08 16:56:31 +0300

    added views

--- server/views.py
+++ server/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from server.models import Team, User, Venue
+from server.serializers import TeamSerializer, VenueSerializer, UserSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class TeamList(APIView):
+    """
+    List all the teams, or create a new team.
+    """
+    def get(self, request, format=None):
+        teams = Team.objects.all()
+        serializer = TeamSerializer(teams, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = TeamSerializer(data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TeamDetail(APIView):
+    """
+    Retrieve, update or delete a team instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Team.objects.get(pk=pk)
+        except Team.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        team = self.get_object(pk)
+        serializer = TeamSerializer(team)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        team = self.get_object(pk)
+        serializer = TeamSerializer(team, data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        team = self.get_object(pk)
+        team.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- streetArtGangs/urls.py
+++ streetArtGangs/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import patterns, include, url
+from rest_framework.urlpatterns import format_suffix_patterns
+from server import views
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'streetArtGangs.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('snippets.urls')),
-
+    url(r'^teams/$', views.TeamList.as_view()),
+    url(r'^teams/(?P<pk>[0-9]+)/$', views.TeamDetail.as_view()),
 )
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
