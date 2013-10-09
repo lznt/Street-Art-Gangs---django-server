4f5da21021defbda0d4c71749c99ca357a8e88ba
Daniele Zanni <syymza@gmail.com>
2013-10-09 16:17:43 +0300

    playing with users and groups

--- server/serializers.py
+++ server/serializers.py
@@ -1,20 +1,19 @@
 from django.forms import widgets
 from rest_framework import serializers
-from server.models import Venue
+from server.models import Venue, Gang
 from django.contrib.auth.models import Group, User
 
 
 class GangSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Group
-        fields = ('id', 'name')
+        model = Gang
+        fields = ('group.id', 'group.name')
 
 
 class GangsterSerializer(serializers.ModelSerializer):
-    gang = serializers.PrimaryKeyRelatedField()
     class Meta:
-        model = User
-        fields = ('id','username', 'gang')
+        model = Gangster
+        fields = ('user.id','user.username')
 
 class VenueSerializer(serializers.ModelSerializer):
     class Meta:
