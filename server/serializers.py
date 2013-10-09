4ac3b4ae60f821b6589754f88b5ca0727387ec65
Daniele Zanni <syymza@gmail.com>
2013-10-09 19:41:01 +0300

    still playing

--- server/serializers.py
+++ server/serializers.py
@@ -5,20 +5,22 @@ from server.models import Venue, Gang, Gangster
 
 class GangSerializer(serializers.ModelSerializer):
 
+    name = serializers.SlugRelatedField(slug_field='name')
     gangsters = serializers.PrimaryKeyRelatedField(many=True)
 
     class Meta:
         model = Gang
-        fields = ('group', 'gangsters')
-        depth = 2
+        fields = ('name', 'gangsters')
 
 
 class GangsterSerializer(serializers.ModelSerializer):
 
+    username = serializers.SlugRelatedField(slug_field='username')
+
     class Meta:
         model = Gangster
-        fields = ('user','gang')
-        depth = 2
+        fields = ('username','gang')
+
 
 
 class VenueSerializer(serializers.ModelSerializer):
