58794d844306549759cc4d09fa86ba605b443504
Daniele Zanni <syymza@gmail.com>
2013-10-09 18:31:54 +0300

    still playing

--- server/serializers.py
+++ server/serializers.py
@@ -4,16 +4,15 @@ from server.models import Venue, Gang, Gangster
 
 
 class GangSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Gang
 
 
 class GangsterSerializer(serializers.ModelSerializer):
-    gang = serializers.PrimaryKeyRelatedField(many=True)
 
     class Meta:
         model = Gangster
-        fields = ('user', 'gang')
 
 class VenueSerializer(serializers.ModelSerializer):
     class Meta:
