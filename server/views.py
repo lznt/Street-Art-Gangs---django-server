096d8892c4c29c4f6bbf0268dee6970503b8d035
Daniele Zanni <syymza@gmail.com>
2013-10-10 17:21:13 +0300

    deletin useless imports

--- server/views.py
+++ server/views.py
@@ -3,10 +3,7 @@ from rest_framework import generics
 from server.models import Venue, Gang, Gangster
 from rest_framework import permissions
 from rest_framework import viewsets
-from rest_framework import renderers
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
+
 
 class GangsterViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Gangster.objects.all()
