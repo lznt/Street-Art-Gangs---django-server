9f15c5e4507bb35ac93c5b7d961c1626d82a14fe
Daniele Zanni <syymza@gmail.com>
2013-10-16 12:11:39 +0300

    Syntax error

--- server/models.py
+++ server/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class UserProfile(models.Model)
+class UserProfile(models.Model):
     user = models.OneToOneField(User)
     tagsCreated = models.IntegerField()
     tagsDeleted = models.IntegerField()
