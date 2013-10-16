7dc42ef264e83d2d603f289cb6bbb8b3799b1c82
Daniele Zanni <syymza@gmail.com>
2013-10-16 12:12:27 +0300

    syntax error fixed

--- server/models.py
+++ server/models.py
@@ -18,7 +18,7 @@ User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
 
 
 
-    class Venue(models.Model):
+class Venue(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, blank=True, null=True)
     time_checkin = models.DateTimeField(blank=True, null=True)
