90f0071f348609198926bd7dc0744c726d2e7a36
Daniele Zanni <syymza@gmail.com>
2013-10-16 12:48:52 +0300

    Added Users view  to urls.py

--- streetArtGangs/urls.py
+++ streetArtGangs/urls.py
@@ -5,6 +5,8 @@ from rest_framework.routers import DefaultRouter
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'venues', views.VenueViewSet)
+router.register(r'gangsters', views.UserViewSet)
+
 
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browseable API.
