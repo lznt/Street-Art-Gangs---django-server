c9a2698aa8f50855721a38bb9d70e07c168b226d
Daniele Zanni <syymza@gmail.com>
2013-10-18 14:09:03 +0300

    Added category

--- server/models.py
+++ server/models.py
@@ -19,11 +19,24 @@ User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
 
 
 class Venue(models.Model):
+    CATEGORY_CHOICES = (
+        ('Outdoors', 'Great Outdoors'),
+        ('Nightlife', 'Nightlife Spots'),
+        ('Aerts', 'Arts & Entertainment'),
+        ('Education', 'Education'),
+        ('Food', 'Food'),
+        ('Other', 'Other Places'),
+        ('Shop', 'Shops & Services'),
+        ('Travel', 'Travel & Transport'),
+    )
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, blank=True, null=True)
     latestEditTimestamp = models.DateTimeField(blank=True, null=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES)
+
+
 
     class Meta:
         ordering = ('-latestEditTimestamp',)
