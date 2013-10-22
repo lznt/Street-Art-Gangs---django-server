84c7ac342d8de2866afb9e4fad1adbb0d557350a
Daniele Zanni <syymza@gmail.com>
2013-10-22 12:39:11 +0300

    disabling apps

--- streetArtGangs/settings.py
+++ streetArtGangs/settings.py
@@ -30,15 +30,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
+#    'django.contrib.admin',
+#    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'server',
-    'south',
+#    'server',
+#    'south',
 )
 
 MIDDLEWARE_CLASSES = (
