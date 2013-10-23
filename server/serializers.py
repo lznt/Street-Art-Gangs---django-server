846117a627cab599e509ded61d034fec0649ef69
Daniele Zanni <syymza@gmail.com>
2013-10-23 12:04:51 +0300

    hide password when getting user

--- server/serializers.py
+++ server/serializers.py
@@ -30,3 +30,14 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'password', 'email')
+
+
+    def restore_object(self, attrs, instance=None):
+        user = super(UserSerializer, self).restore_object(attrs, instance)
+        user.set_password(attrs['password'])
+        return user
+
+    def to_native(self, obj):
+        ret = super(UserSerializer, self).to_native(obj)
+        del ret['password']
+        return ret
