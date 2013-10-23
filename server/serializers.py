d09611ff113869171146a842225da34750fa0a28
Daniele Zanni <syymza@gmail.com>
2013-10-23 16:51:29 +0300

    trying to show the geng in the user serializer

--- server/serializers.py
+++ server/serializers.py
@@ -27,7 +27,7 @@ class GangSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
 
-	gang = serializers.Field(source = 'profile.gang')
+	gang = serializers.ModelField(model_field=User()._meta.get_field('profile.gang'))
 
 	class Meta:
 		model = User
