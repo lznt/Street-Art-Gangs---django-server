6cae1a8396a1a47b766cd9c7e7a1514efd8c0db1
Daniele Zanni <syymza@gmail.com>
2013-10-24 19:13:13 +0300

    playing with routers

--- server/admin.py
+++ server/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from server.models import Venue, UserProfile
+from server.models import Venue, UserProfile, Gang
 
 admin.site.register(Venue)
 admin.site.register(UserProfile)
+admin.site.register(Gang)
