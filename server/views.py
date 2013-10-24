3836f3d7d0e52ddf424f6112d74a3a64a441418f
Daniele Zanni <syymza@gmail.com>
2013-10-24 19:37:35 +0300

    added missing import

--- server/views.py
+++ server/views.py
@@ -2,6 +2,8 @@
 from rest_framework import viewsets
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework import status
+
 
 #Authentication and Permissions
 from rest_framework.permissions import IsAuthenticated
