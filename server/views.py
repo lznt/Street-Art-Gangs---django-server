ba16e55fdee4ca793307dacdef0c98121135fc5d
Daniele Zanni <syymza@gmail.com>
2013-10-24 19:54:43 +0300

    setting priorities to users

--- server/views.py
+++ server/views.py
@@ -17,56 +17,56 @@ from django.contrib.auth.models import User
 from server.serializers import VenueSerializer, UserProfileSerializer, GangSerializer, UserSerializer
 
 class VenueViewSet(viewsets.ModelViewSet):
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsAuthenticated,)
-    queryset = Venue.objects.all()
-    serializer_class = VenueSerializer
+	authentication_classes = (SessionAuthentication, BasicAuthentication)
+	permission_classes = (IsAuthenticated,)
+	queryset = Venue.objects.all()
+	serializer_class = VenueSerializer
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsAuthenticated,)
-    queryset = UserProfile.objects.all()
-    serializer_class = UserProfileSerializer
+	authentication_classes = (SessionAuthentication, BasicAuthentication)
+	permission_classes = (IsAuthenticated,)
+	queryset = UserProfile.objects.all()
+	serializer_class = UserProfileSerializer
 
 class GangViewSet(viewsets.ModelViewSet):
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsAuthenticated,)
-    queryset = Gang.objects.all()
-    serializer_class = GangSerializer
+	authentication_classes = (SessionAuthentication, BasicAuthentication)
+	permission_classes = (IsAuthenticated,)
+	queryset = Gang.objects.all()
+	serializer_class = GangSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsAuthenticated,)
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+	authentication_classes = (SessionAuthentication, BasicAuthentication)
+	permission_classes = (IsAuthenticated,)
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
 
 
 
 class UserRegistrerView(APIView):
 
-    """
-    Create a new User.
-    """
+	"""
+	Create a new User.
+	"""
 
-    def get(self, request, *args, **kwargs):
+	def get(self, request, *args, **kwargs):
 
-        # GET not allowed
-        errors = dict()
-        return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+		# GET not allowed
+		errors = dict()
+		return Response(errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
-    def post(self, request, format=None):
-        user_profile_serializer = UserProfileSerializer(
-                data=request.DATA)
-        user_serializer = UserSerializer(
-                data=request.DATA)
-        if user_profile_serializer.is_valid() and user_serializer.is_valid():
-            user_serializer.save()
-            user_profile_serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # Combine errors from both serializers.
-        errors = dict()
-        errors.update(user_profile_serializer.errors)
-        errors.update(user_serializer.errors)
-        return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+	def post(self, request, format=None):
+		user_serializer = UserSerializer(
+				data=request.DATA)
+		if user_serializer.is_valid():
+			user = user_serializer.save()
+			user_profile_serializer = UserProfileSerializer(instance=user,
+				data=request.DATA)
+			if user_profile_serializer.is_valid()
+				user_profile_serializer.save()
+				return Response(serializer.data, status=status.HTTP_201_CREATED)
+		# Combine errors from both serializers.
+		errors = dict()
+		errors.update(user_profile_serializer.errors)
+		errors.update(user_serializer.errors)
+		return Response(errors, status=status.HTTP_400_BAD_REQUEST)
