8a3244edb3e77dbd8e64ce7f77e605eda75f9422
Daniele Zanni <syymza@gmail.com>
2013-10-24 20:02:32 +0300

    setting user id in data

--- server/views.py
+++ server/views.py
@@ -60,8 +60,9 @@ class UserRegistrerView(APIView):
 				data=request.DATA)
 		if user_serializer.is_valid():
 			user = user_serializer.save()
-			user_profile_serializer = UserProfileSerializer(instance=user,
-				data=request.DATA)
+			data = request.DATA
+			data.push('user', user.id)
+			user_profile_serializer = UserProfileSerializer(data=data)
 			if user_profile_serializer.is_valid():
 				user_profile_serializer.save()
 				return Response(serializer.data, status=status.HTTP_201_CREATED)
