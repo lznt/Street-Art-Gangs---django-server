ec2395b64c842a834388a88c9d1f0568e101aa0f
Daniele Zanni <syymza@gmail.com>
2013-10-24 20:09:30 +0300

    added missing import

--- server/views.py
+++ server/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.utils.decorators import method_decorator
 
 
 #Authentication and Permissions
