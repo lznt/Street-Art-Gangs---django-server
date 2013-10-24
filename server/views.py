db52540b04a72aab12bbc172987f6a8b2adfdaf2
Daniele Zanni <syymza@gmail.com>
2013-10-24 20:55:37 +0300

    Edited Response

--- server/views.py
+++ server/views.py
@@ -60,12 +60,12 @@ class UserRegistrerView(APIView):
 		if user_serializer.is_valid():
 			user = user_serializer.save()
 			data = request.DATA.copy()
-			data['user'] = user.id
+			data['user'] = user_serializer.data.id
 			user_profile_serializer = UserProfileSerializer(data=data)
 
 			if user_profile_serializer.is_valid():
 				user_profile_serializer.save()
-				return Response(serializer.data, status=status.HTTP_201_CREATED)
+				return Response(user_profile_serializer.data, status=status.HTTP_201_CREATED)
 
 			return Response(user_profile_serializer.errors, status=status.HTTP_400_BAD_REQUEST
 
