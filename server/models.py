38fdc0852af7a9505bc86b786692726b32ecbab1
Daniele Zanni <syymza@gmail.com>
2013-10-29 10:56:25 +0200

    Adding acted_recently property to profile

--- server/models.py
+++ server/models.py
@@ -1,18 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+import datetime
+from django.utils import timezone
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     gang = models.ForeignKey('Gang', related_name='gangsters')
-    tagsCreated = models.IntegerField(default=0)
-    tagsDeleted = models.IntegerField(default=0)
+    tags_created = models.IntegerField(default=0)
+    tags_deleted = models.IntegerField(default=0)
     money = models.IntegerField(default=0)
     latitude = models.FloatField(blank=True, null=True)
     longitude = models.FloatField(blank=True, null=True)
+    last_action = models.DateTimeField(blank=True, null=True)
 
     def __unicode__(self):
         return self.user.username
+
+    def _acted_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(seconds=30) <= self.last_action <  now
+
+    acted_recently = property(_acted_recently)
 
 
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
--- server/serializers.py
+++ server/serializers.py
@@ -10,7 +10,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = UserProfile
-		fields = ('id', 'user', 'username','latitude', 'longitude', 'tagsCreated', 'tagsDeleted', 'money', 'gang', 'color')
+		fields = ('id', 'user', 'username','latitude', 'longitude', 'tags_created', 'tags_deleted', 'money', 'last_action', 'acted_recently','gang', 'color')
 
 
 class GangSerializer(serializers.ModelSerializer):
